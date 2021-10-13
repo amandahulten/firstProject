d33929311dc19e17600ac793b6e7c8177c6b7411
amandahulten <amahul1029@skola.goteborg.se>
2021-10-13 10:35:47 +0200

    Added file-structure

--- index.php
+++ index.php
@@ -1,1 +1,9 @@
-<?= 'The Elephpent in the Room'; ?>
+<?php require __DIR__ . '/php/header.php'; ?>
+<?php require __DIR__ . '/php/functions.php'; ?>
+
+<header>
+    <h1>Hello</h1>
+</header>
+
+
+<?php require __DIR__ . '/php/footer.php'; ?>
